077910fe1d88c1ee68069b169ffa298e1773c401
matta-roopa <53473369+matta-roopa@users.noreply.github.com>
2019-12-24 08:13:48 +0530

    Add files via upload

--- array.java
+++ array.java
@@ -7,17 +7,20 @@ public static void main(String[] args)
 Scanner s=new Scanner(System.in);
 int n=s.nextInt();
 int[] a=new int[n];
-int p=0;
+
 for(int i=0;i<n;i++)
 {
 a[i]=s.nextInt();
 }
+int p=a[0],m=a[0];
 for(int i=0;i<n;i++)
 {
-p=p+a[i];
+if(p<a[i])
+p=a[i];
+if(m>a[i])
+m=a[i];
 }
-System.out.println("sum "+p);
-int l=p/n;
-System.out.println("AVERAGE"+l);
+
+System.out.println("MIN:"+m+"MAX:"+p);
 }
 }
